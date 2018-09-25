1317c2fb1ac32e2668b1f8243e4af01f9dc0e0e9
803142 <803142@gmail.com>
2018-09-25 21:59:16 +0300

    TASK 6 solve with stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let resultStack=[],
+      openElement=0;
+   for (let i=0, lenS=str.length; i<lenS; i++){
+    for (const [x,y] of bracketsConfig){
+      const z = str[i];
+      const s = resultStack[openElement-1];
+      const condtnOne = z == y;
+      const condtnTwo = s == x;
+      const condtnThree = z == x; 
+      if (condtnOne) {
+        if (condtnTwo) {
+         resultStack.pop();
+         openElement--; 
+        } 
+        else{
+        resultStack.push(z);
+        openElement++;
+        }
+      }
+     else {
+        if (condtnThree){
+          resultStack.push(z);
+          openElement++;
+        }        
+     }
+    }
+  }
+  
+  return !(resultStack.length>0);
 }
