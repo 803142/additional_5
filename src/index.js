dcd09a3ee7c965e3feaa9650aec339664fa261b4
803142 <803142@gmail.com>
2019-03-20 12:55:25 +0300

    style: more clean view

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ module.exports = function check(str, bracketsConfig) {
   let resultStack=[];
   let openElement=0;
 
-  const s = resultStack[openElement-1];
-
-    for (let i=0, lenS=str.length; i<lenS; i++){
-      for (const [x,y] of bracketsConfig){
-        if (str[i] == y) {
-          if (s == x) {
-            resultStack.pop();
-            openElement--; 
-          }else {
-            resultStack.push(z);
-            openElement++;
-          }
+  for (let i=0, lenS=str.length; i<lenS; i++){
+    for (const [x,y] of bracketsConfig){
+      if (str[i] == y) {
+        if (resultStack[openElement-1] == x) {
+          resultStack.pop();
+          openElement--; 
         }else {
-          if (str[i] == x){
-            resultStack.push(z);
-            openElement++;
-          }        
+          resultStack.push(str[i]);
+          openElement++;
         }
+      }else {
+        if (str[i] == x){
+          resultStack.push(str[i]);
+          openElement++;
+        }        
       }
     }
-  
+  }
+
   return !(resultStack.length);
+
 }
